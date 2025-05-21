f83673d3a76389bff29fa8f9389e25effd3f35ab
Rean Tsai <aobc55625@gmail.com>
2025-05-21 10:04:51 +0800

    Vue 實務案例：API 串接後端資料

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,28 @@
 <script setup>
-  import { ref } from 'vue';
-  import MainComp from './Main.vue';
+  import { onMounted, ref } from 'vue';
+  import axios from 'axios';
+  import List from './List.vue';
 
-  const visible = ref(true);
-  const hide = function() {
-    visible.value = false;
-  };
-  const show = function() {
-    visible.value = true;
-  };
+  const products = ref(null);
+  const url = 'https://cwpeng.github.io/live-records-samples/data/products.json';
+  onMounted(async function() {
+    const resp = await axios.get(url);
+    console.log(resp);
+    const { data } = resp;
+    console.log(data);
+    products.value = data;
+  });
 </script>
 
 <template> 
-  <MainComp v-if="visible"></MainComp>
-  <button @click="hide">隱藏</button>
-  <button @click="show">重新顯示</button>
+  <div class="headline">產品資料列表</div>
+  <div v-if="products === null">資料載入中</div>
+  <List :products="products || []" v-else></List>
 </template>
 
 <style scoped> 
-  button {
-    border: 1px solid black;
-    background-color: aliceblue;
-    color: black;
-  }
-
-  button:hover {
-    opacity: 0.7;
-  }
-
-  button:active {
-    opacity: 0.5;
-  }
-</style>
+.headline {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style> 
---
+++ src/List.vue
@@ -0,0 +1,28 @@
+<script setup>
+  defineProps(['products']);
+</script>
+
+<template> 
+  <div class="list">
+    <div class="product" v-for="product in products">
+      <div class="name"> {{ product.name }} </div>
+      <div class="description"> {{ product.description }} </div>
+      <div class="price">單價: {{ product.price }} </div>
+    </div>
+  </div>
+</template>
+
+<style scoped> 
+  .list {
+  line-height: 1.5rem;
+}
+
+.product {
+  padding: 15px 0;
+  border-bottom: 1px solid black;
+}
+
+.product .name {
+  font-weight: bold;
+}
+</style> 
